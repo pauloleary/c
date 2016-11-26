532df6e6a2137d429de012e63fa0c328f6f9c567
Paul O'Leary <paul.o-leary@sap.com>
2016-11-26 10:05:10 +0000

    Updates to linked list header file

--- linked-list/linked_list.h
+++ linked-list/linked_list.h
@@ -4,11 +4,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+/*
+ * Struct representing a node in
+ * in the linked list
+ */
+typedef struct list_node {
+
+	int value;
+	struct list_node * node;
+
+} list_node;
+
 /* 
  * Struct representing a linked list 
  */
 typedef struct linked_list {
 
+	int legth;
+	list_node * head;
 
 } linked_list;
 
