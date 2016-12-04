fdacb49ba24ee3f3a91bf2eab1e3d2070bbc9325
Paul O'Leary <paul.o-leary@sap.com>
2016-12-04 22:03:10 +0000

    Template files for linked list backed queue

---
+++ queue/list_queue.h
@@ -0,0 +1,8 @@
+#ifndef LIST_QUEUE_H_
+#define LIST_QUEUE_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#endif 
+
---
+++ queue/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "list_queue.h"
+
+
+int main (void) {
+
+	return 0;
+
+}
+
+
