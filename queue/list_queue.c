df1704510dd05680d80850d104c23165bd1d99dd
Paul O'Leary <paul.o-leary@sap.com>
2016-12-07 20:53:03 +0000

    List structure and function signatures

--- queue/list_queue.c
+++ queue/list_queue.c
@@ -1,3 +1,22 @@
 #include "list_queue.h"
 
+/**
+ * Add an item to the list
+ */
+void enqueue(queue * queue, int value) {
+	
+} 
 
+/**
+ * Remove the next item from the list
+ */
+int dequeue(queue * queue) {
+	return 1;
+} 
+
+/**
+ * Is the list empty
+ */
+bool empty(queue * queue) {
+	return 0;	
+}
--- queue/list_queue.h
+++ queue/list_queue.h
@@ -3,6 +3,46 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+
+/*
+ * Struct representing a node in
+ * in the linked list
+ */
+typedef struct queue_node {
+
+	int value;
+	struct queue_node * next;
+
+} queue_node;
+
+/* 
+ * Struct representing a linked list 
+ */
+typedef struct queue {
+
+	int length;
+	queue_node * head;
+	queue_node * tail;
+
+} queue;
+
+/**
+ * Add an item to the list
+ */
+void enqueue(queue * queue, int value); 
+
+/**
+ * Remove the next item from the list
+ */
+int dequeue(queue * queue); 
+
+/**
+ * Is the list empty
+ */
+bool empty(queue * queue);
+
+
 
 #endif 
 
